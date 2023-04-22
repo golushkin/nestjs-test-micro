0aa4a2a493e39032b245adc81b3928dda458a89a
Andew Golushkin <a.golushkin@softteco.com>
2023-04-22 20:04:00 +0300

    added validation

--- validation/src/app/app.controller.ts
+++ validation/src/app/app.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { OrderEntity } from './entities/orders.entity';
+import { ContainUserIdGuard } from './guards/contain-user-id.guard';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { Request } from 'express';
 
-@Controller()
+@Controller('orders')
+@UseGuards(ContainUserIdGuard)
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post()
+  async create(
+    @Body() info: CreateOrderDto,
+    @Req() req: Request,
+  ): Promise<OrderEntity> {
+    const userId = req.headers['x-user-id'] as string;
+    return await this.appService.createOder(info, userId);
+  }
 }
--- validation/src/app/app.service.ts
+++ validation/src/app/app.service.ts
@@ -1,7 +1,14 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { ORDER_PROVIDER } from './providers/orders.provider';
 import { CollectionReference } from '@google-cloud/firestore';
 import { OrderEntity } from './entities/orders.entity';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { generateId } from '../utils/id';
+import { OrderStatusEnum } from './enums/order-status.enum';
 
 @Injectable()
 export class AppService {
@@ -9,4 +16,24 @@ export class AppService {
     @Inject(ORDER_PROVIDER)
     private readonly orders: CollectionReference<OrderEntity>,
   ) {}
+
+  async createOder(info: CreateOrderDto, userId: string): Promise<OrderEntity> {
+    const docId = generateId();
+    const docRef = this.orders.doc(docId);
+    await docRef.set({
+      ...info,
+      _id: docId,
+      userId,
+      status: OrderStatusEnum.CREATED,
+    });
+
+    const doc = await docRef.get();
+    const order = doc.data();
+
+    if (!order) {
+      throw new InternalServerErrorException();
+    }
+
+    return order;
+  }
 }
